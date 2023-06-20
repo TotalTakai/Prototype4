70d6af90f3e1f8d812d439403ba6b9977c98261e
Nir <yanaynir1@gmail.com>
2023-06-20 14:02:08 +0300

    Fixed RocketBehavior Script
    Fix the rockets behavior script to make sure the rockets spawn and move correctly,
    still have work to do

--- Assets/Script/Powerups.cs
+++ Assets/Script/Powerups.cs
@@ -2,8 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PowerupType { None, Pushback, Rockets };
+
 public class Powerups : MonoBehaviour
 {
+    public PowerupType powerupType;
+    
     // Start is called before the first frame update
     void Start()
     {
